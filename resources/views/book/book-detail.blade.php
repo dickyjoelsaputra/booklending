d0ce90be893f801e21440421f3d6a5c5f16f2339
Dicky Joel Saputra <115436194+dickyjoelsaputra@users.noreply.github.com>
2023-01-08 19:43:51 +0700

    Update book-detail.blade.php

--- resources/views/book/book-detail.blade.php
+++ resources/views/book/book-detail.blade.php
@@ -6,5 +6,26 @@
 @endsection
 
 @section('content')
-    {{ $book }}
+    <p>Name : {{ $book->name }}</p>
+    <p>Author : {{ $book->author }}</p>
+    <p>Publisher: {{ $book->publisher }}</p>
+    <p>Release Year: {{ $book->release }}</p>
+    <p>Description: {{ $book->description }}</p>
+    <p>Categories:</p>
+    <ul>
+        @foreach ($book->categories as $category)
+            <li>{{ $category->name }}</li>
+        @endforeach
+    </ul>
+
+    @if ($book->image != null)
+        <img class="img-thumbnail" src="{{ asset('storage/books/' . $book->image) }}" alt="" width="200px">
+    @else
+        <img class="img-thumbnail"
+            src="https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg"
+            alt="" width="200px">
+    @endif
+
+
+
 @endsection
